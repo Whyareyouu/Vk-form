c8bc661f38258c55f388d167bc77d66e92842a14
Whyareyou_ <54592995+Whyareyouu@users.noreply.github.com>
2023-05-08 20:23:02 +0300

    Update useFormState.ts

--- src/hooks/useFormState.ts
+++ src/hooks/useFormState.ts
@@ -4,7 +4,7 @@ import { FormStateContext } from '../ui/context/context';
 function useFormState() {
 	const context = React.useContext(FormStateContext);
 	if (context === undefined) {
-		throw new Error('useCountState must be used within a CountProvider');
+		throw new Error('useCountState must be used within a FormStateProvider');
 	}
 	return context;
 }
